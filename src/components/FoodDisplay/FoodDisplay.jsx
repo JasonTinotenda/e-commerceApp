afe377c393cc4c478fc53bcda005b2c455e2c5d4
JasonTinotenda <kufakwatenziyason@gmail.com>
2024-05-31 10:14:11 +0200

    Rest of the sections

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,7 +3,7 @@ import { StoreContext } from '../../context/StoreContext';
 import './FoodDisplay.css';
 import FoodItem from '../FoodItem/FoodItem';
 
-const FoodDisplay = ({ category }) => {
+const FoodDisplay = ({ category}) => {
   const { food_list } = useContext(StoreContext);
 
   return (
@@ -11,6 +11,9 @@ const FoodDisplay = ({ category }) => {
       <h2>Top dishes near you</h2>
       <div className='food-display-list'>
         {food_list.map((item, index) => {
+          {console.log(category,item.category);}
+          if (category==="All" || category===item.category){
+            const name = typeof item.name === 'object' ? JSON.stringify(item.name) : item.name;
           const description = typeof item.description === 'object' ? JSON.stringify(item.description) : item.description;
           const price = typeof item.price === 'object' ? JSON.stringify(item.price) : item.price;
           const image = typeof item.image === 'object' ? JSON.stringify(item.image) : item.image;
@@ -19,11 +22,14 @@ const FoodDisplay = ({ category }) => {
             <FoodItem
               key={index}
               id={item._id}
+              name={name}
               description={description}
               price={price}
               image={image}
             />
           );
+          }
+          
         })}
       </div>
     </div>
