ea8d255d93d02576392f7d5df2564f0105325e11
JasonTinotenda <kufakwatenziyason@gmail.com>
2024-05-31 14:39:18 +0200

    cart- update

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,27 +1,26 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import {StoreContext} from '../../context/StoreContext'
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
-  const {cartIterms,food_list,removeFromCart} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  
   return (
     <div className='cart'>
-      <div className="cart-iteems">
-        <div className="cart-items title">
+      <div className="cart-items">
+        <div className="cart-items-title">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
-          <p>Quantinty</p>
+          <p>Quantity</p>
           <p>Total</p>
           <p>Remove</p>
         </div>
       </div>
       <br/>
-      <hr/>
-      
-      
+      <hr/>      
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
